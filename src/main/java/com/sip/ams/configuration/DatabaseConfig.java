8de7c33509d76c498790ad5296a3b7c10c4a16c8
rihabkamel <rihab.kamel.90@gmail.com>
2021-04-10 14:21:47 +0200

    version 3

--- src/main/java/com/sip/ams/configuration/DatabaseConfig.java
+++ src/main/java/com/sip/ams/configuration/DatabaseConfig.java
@@ -1,13 +1,13 @@
 package com.sip.ams.configuration;
 
 import com.zaxxer.hikari.*;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
+
 	@Value("${spring.datasource.url}")
 	private String dbUrl;
 
